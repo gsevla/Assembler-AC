e3de9b9f07a68e87a7d78bddc63929babcaa247c
Gabriel de Sousa Alves <sousa.alves@aluno.uece.br>
2017-12-05 13:25:48 -0300

    Já fiz minha parte, agora faz a tua!

--- Codigo/assembler.py
+++ Codigo/assembler.py
@@ -4,12 +4,18 @@ import sys
 # String de paramentro na chamada no terminal
 file_name = sys.argv[1]
 
-# Acessa um arquivo para leitura
-arq = open(file_name, 'r')
+if(file_name.endswith(".asm")):	# Verfica se o arquivo informado tem a extensão correta (ACHAR UM FORMA DE INTERROMPER O PROGRAMA)
+	try:
+		# Acessa um arquivo para leitura
+		arq = open(file_name, 'r')
 
-# Lê cada linha do arquivo e transforma em uma lista de strings
-lista = arq.readlines()
+		# Lê cada linha do arquivo e transforma em uma lista de strings
+		lista = arq.readlines()
 
-for l in lista:
-	print(l[:-1])
+		for l in lista:
+			print(l[:-1])
 
+	except FileNotFoundError:
+		print("O arquivo informado não existe!")
+else:	# Finaliza o programa caso a extensão seja incorreta
+	print("O arquivo informado não é válido! (!= .asm)")
